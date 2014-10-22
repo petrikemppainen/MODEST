ce134ed68b5ef87d1f3bb13ff5740c148bbabb63
Petri Kemppainen <petrikemppainen2@gmail.com>
2014-10-22 12:26:00 +0200

    build

---
+++ R/MODEST.R
@@ -0,0 +1,10 @@
+#' MODEST
+#'
+#' MODEST integrates results across replicated STRUCTURE runs spanning a large rang of K values and therefore takes into account multimodality of data. change
+#' 
+#' More info to come
+#' 
+#' @author Petri Kemppainen, \email{petrikemppainen2@@gmail.com} and Stuart Baird, \email{stuartj.e.baird@@gmail.com}
+#' @docType package
+#' @name MODEST
+NULL
